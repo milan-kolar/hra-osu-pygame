dd1d1697058a8e9db84b00fd5321f93045402784
Milan Kolář <minosek.kolar@gmail.com>
2023-01-24 21:00:03 +0100

    Časovač, počátek zkracování

--- Game OSU.py
+++ Game OSU.py
@@ -7,8 +7,33 @@ pygame.init()
 # Okno hry o velikosti 640x480 pixelů
 screen = pygame.display.set_mode((640, 480))
 
+# Časovač
+clock = pygame.time.Clock()
+current_time = 0
+ball_screen_time = 0
+
 # Titulek
 pygame.display.set_caption("Sestřelit míček")
+
+# Metoda vykreslování míčku a skóre
+def ball_and_score():
+    # Pozadí okna RGB formát = vymažeme předchozí obsah okna
+    screen.fill((255, 255, 255))
+                    
+    x = random.randint(0, 640)
+    y = random.randint(0, 480)
+
+    # Vykreslíme nový míček (na náhodné pozici z IF) a skóre
+    ball = pygame.draw.circle(screen, (0, 0, 255), (x,y), 50)
+    score_text = f"Score: {score}"
+    score_font = pygame.font.Font(None, 36)
+    score_surface = score_font.render(score_text, True, (0, 0, 0))
+    screen.blit(score_surface, (20, 20))
+
+    # Čas míčku na obrazovce
+    ball_screen_time = pygame.time.get_ticks()
+    print(ball_screen_time)
+    return ball_screen_time
 
 # Pozadí okna RGB formát
 screen.fill((255, 255, 255))
@@ -21,6 +46,9 @@ x = random.randint(0, 640)
 y = random.randint(0, 480)
 # povrch na vykreslení, barva RGB, souřadnice středu, poloměr
 ball = pygame.draw.circle(screen, (0, 0, 255), (x, y), 50)
+
+# Čas míčku na obrazovce
+ball_screen_time = pygame.time.get_ticks()
 
 # Vykreslení skóre
 score_text = f"Score: {score}"
@@ -52,17 +80,48 @@ while running:
                     running = False
                 # Jinak nový míček na náhodné pozici
                 else:
+                    # Pozadí okna RGB formát = vymažeme předchozí obsah okna
+                    screen.fill((255, 255, 255))
+                    
                     x = random.randint(0, 640)
                     y = random.randint(0, 480)
-    # Pozadí okna RGB formát = vymažeme předchozí obsah okna
-    screen.fill((255, 255, 255))
 
-    # Vykreslíme nový míček a skóre
-    ball = pygame.draw.circle(screen, (0, 0, 255), (x,y), 50)
-    score_text = f"Score: {score}"
-    score_font = pygame.font.Font(None, 36)
-    score_surface = score_font.render(score_text, True, (0, 0, 0))
-    screen.blit(score_surface, (20, 20))
+                    # Vykreslíme nový míček (na náhodné pozici z IF) a skóre
+                    ball = pygame.draw.circle(screen, (0, 0, 255), (x,y), 50)
+                    score_text = f"Score: {score}"
+                    score_font = pygame.font.Font(None, 36)
+                    score_surface = score_font.render(score_text, True, (0, 0, 0))
+                    screen.blit(score_surface, (20, 20))
+
+                    # Čas míčku na obrazovce
+                    ball_screen_time = pygame.time.get_ticks()
+    
+    # if current_time - ball_screen_time > 1000:
+    #     # Pozadí okna RGB formát = vymažeme předchozí obsah okna
+    #     screen.fill((255, 255, 255))
+                    
+    #     x = random.randint(0, 640)
+    #     y = random.randint(0, 480)
+
+    #     # Vykreslíme nový míček (na náhodné pozici z IF) a skóre
+    #     ball = pygame.draw.circle(screen, (0, 0, 255), (x,y), 50)
+    #     score_text = f"Score: {score}"
+    #     score_font = pygame.font.Font(None, 36)
+    #     score_surface = score_font.render(score_text, True, (0, 0, 0))
+    #     screen.blit(score_surface, (20, 20))
+
+    #     # Čas míčku na obrazovce
+    #     ball_screen_time = pygame.time.get_ticks()
+    #     print(ball_screen_time)
+
+    if current_time - ball_screen_time > 1000:
+        ball_screen_time = ball_and_score()
+
+    # Měření aktuálního času
+    current_time = pygame.time.get_ticks()
 
      # Aktualizace okna
     pygame.display.flip()
+
+    # Neznámý řádek, PŘIČÍTÁ MILISEKUNDY??, 1frame = 17ms
+    clock.tick(60)
